255c0d3f6b8f4e8b69e8f03bef74988b4b24948f
Jared Long <jlong4223@gmail.com>
2021-11-18 13:46:07 -0600

    feat: updates file removal to include conditional

--- bin/eslint-staged.js
+++ bin/eslint-staged.js
@@ -1,31 +1,32 @@
 #!/usr/bin/env node
+"use strict";
 
 let fs = require("fs");
 let shell = require("shelljs");
 const chalk = require("chalk");
 
+const stagedFile = "staged.txt";
 let args;
 [, , ...args] = process.argv;
 
 // Check for user provided file and provide confirmation of file name
 if (args.length !== 0) {
-  console.log("Your provided file name is...", args);
+  console.log(`Your provided file name is... ${args}`);
 }
 
 // If the user doesnt provide a file to check, then one will be created
 if (args.length === 0) {
   console.log(
-    `No file provided, creating or using existing file: ${chalk.blue(
+    `No file provided. Creating or using existing file: ${chalk.blue(
       "staged.txt"
     )}`
   );
-  shell.exec("git diff --cached --name-only > staged.txt");
-  args = ["staged.txt"];
+  shell.exec(`git diff --cached --name-only > ${stagedFile}`);
+  args = [stagedFile];
 }
 
-// remove the provided args from the array
+// remove the provided arguments from the array
 const files = args.shift();
-
 console.log(`Checking ${chalk.blue(files)} for staged changes...`);
 
 // Intitializing the readFileLines with the file
@@ -42,14 +43,20 @@ if (arrOfFiles.length !== 0) {
   console.log(`Staged Files Found: ${chalk.green(arrOfFiles)}`);
 
   let stringFilesToCheck = arrOfFiles.join(" ");
-  console.log(chalk.green(`Linting in progress ===> ${stringFilesToCheck}`));
+  console.log(
+    chalk.blue(`Linting in progress >>>> 🕵️‍♂️ ${stringFilesToCheck} 👀`)
+  );
 
   const executableScript = `npx eslint ${stringFilesToCheck}`;
   shell.exec(executableScript);
-  console.log(chalk.green("Linting complete"));
+
+  console.log(chalk.green("Linting complete ✅"));
 } else {
   console.log(chalk.red("No Staged Files Found - git add files and retry"));
 }
 
-console.log(chalk.green("Removing staged.txt..."));
-shell.exec("rm -rf staged.txt");
+// Remove the generated file - this will not remove a file created by the user if they passed one as an argument
+if (files === stagedFile) {
+  console.log(chalk.italic.green(`Removing generated file: ${stagedFile}`));
+  shell.exec(`rm -rf ${stagedFile}`);
+}
